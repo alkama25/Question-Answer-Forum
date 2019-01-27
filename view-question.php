0dfad976899632d4836d11a38ec0506e1cd71b22
alkama25 <46832736+alkama25@users.noreply.github.com>
2019-01-27 19:09:17 +0530

    Create view-question.php

---
+++ view-question.php
@@ -0,0 +1,73 @@
+<?php
+include 'header.php';
+include 'q&aforumdb.php';
+$data=$conn->prepare("select * from fquestion");
+$data->execute();
+$result=$data->fetchall(PDO::FETCH_OBJ);
+?>
+	<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        User Profile
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Examples</a></li>
+        <li class="active">User profile</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+                <section class="content">
+                    <div class="row">
+                        <div class="col-sm-12">
+                    <div class="box">
+                    <div class="box-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Title of Question</th>
+                                    <th>Description</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                foreach($result as $row){?>
+
+                                <tr>
+                                    <td><?php echo $row->title_of_question;?></td>
+                                    <td><?php echo $row->description;?></td>
+                                
+                                
+                                    <td>
+                                        <a href="view-answer.php?id=<?php echo $row->id ?>" class="btn btn-primary">View All answers</a>
+                                        
+                                        <a href="give-answer.php?id=<?php echo $row->id ?>" class="btn btn-success">Give Answer</a>
+                                    </td>
+                                    
+                                    
+                                </tr>
+
+                                <?php 
+                            }
+                                ?>
+                            </tbody>
+                        </table>
+                	
+            </div>
+            </div>
+            </div>
+            </div>
+                 	
+                 	
+                 
+                </section>
+    <!-- /.content -->
+  </div>
+
+
+
+
